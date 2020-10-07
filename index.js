e1bf5b8c6ed72d48dd1278e24c34e571920b7fde
Fabiano Brito <fabiano.brito@wolt.com>
2020-10-07 23:09:27 +0300

    Check for free slots and notify if existing

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 const axios = require('axios').default;
+const notifier = require('node-notifier');
 
-// Meilahti
-// (async () => {
-//   const response = await axios.get('https://meilahti.slsystems.fi/booking/booking-calendar?BookingCalForm%5Bp_laji%5D=1&BookingCalForm%5Bp_pvm%5D=2020-10-18&BookingCalForm%5Bp_pvm_interval%5D=&BookingCalForm%5Bp_pvm_custom%5D=sunnuntai+18.10.2020');
-//   const freeSlotElements = response.data.match(/\d+\:\d+<br><strong>Varaa<\/strong><\/a>/g);
-//   const freeSlots = freeSlotElements.map(el => el.replace(/<br><strong>Varaa<\/strong><\/a>/, ''));
-//   console.log('free slots at 09:00', freeSlots.filter(slot => slot === '09:00').length);
-//   console.log('free slots at 10:00', freeSlots.filter(slot => slot === '10:00').length);
-// })();
+const date = '2020-10-25';
 
-const date = '2020-10-18';
+const nine = '09:00';
+const ten = '10:00';
 
 const scrapeTapiola = async () => {
   const response = await axios.get(`https://vj.slsystems.fi/tennispuisto/ftpages/ft-varaus-table-01.php?laji=1&pvm=${date}&goto=0`);
   const freeSlotElements = response.data.match(/Te\d+<br>\d+:\d+/g);
   const freeSlots = freeSlotElements.map(el => el.split('<br>'));
-  console.log(freeSlots)
+
+  const nineSlots = freeSlots.filter(slot => slot[1] === nine).length;
+  const tenSlots = freeSlots.filter(slot => slot[1] === ten).length;
+
+  if (nineSlots >= 2 && tenSlots >= 2) {
+    notifier.notify({
+      title: 'Courts available!',
+      message: 'There are courts available on 2020-10-25',
+      open: 'https://vj.slsystems.fi/tennispuisto',
+      timeout: 5,
+    });
+
+    notifier.on('timeout', function (notifierObject, options) {
+      console.log(options);
+      // install open to open the browser
+      // yarn add open
+      // const open = require('open');
+      // open(options.open);
+    });
+  }
+  
+  console.log(`\n::: Slots in Tapiola on ${Date()}`);
+  console.log(freeSlots, '\n')
 }
 
 const tenMinutes = 1000 * 60 * 10;
 scrapeTapiola();
-setInterval(scrapeTapiola, tenMinutes);
-
-// use https://github.com/mikaelbr/node-notifier#readme for notification
+// setInterval(scrapeTapiola, tenMinutes);
