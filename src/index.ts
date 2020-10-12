addcfb126b353d16eb8845919b7d49026865e284
Fabiano Brito <fabiano.brito@wolt.com>
2020-10-12 18:08:46 +0300

    Refactor index to TS

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-const axios = require('axios').default;
-const notifier = require('node-notifier');
-const open = require('open');
+import axios from 'axios';
+import notifier from 'node-notifier';
+import open from 'open';
 
-const date = '2020-10-11';
+const date = '2020-10-18';
 
 const nine = '09:00';
 const ten = '10:00';
 
 const scrapeTapiola = async () => {
-  const response = await axios.get(`https://vj.slsystems.fi/tennispuisto/ftpages/ft-varaus-table-01.php?laji=1&pvm=${date}&goto=0`);
+  const response = await axios.get<string>(`https://vj.slsystems.fi/tennispuisto/ftpages/ft-varaus-table-01.php?laji=1&pvm=${date}&goto=0`);
   const freeSlotElements = response.data.match(/Te\d+<br>\d+:\d+/g);
-  const freeSlots = freeSlotElements.map(el => el.split('<br>'));
+  const freeSlots = (freeSlotElements || []).map(el => el.split('<br>'));
 
   const nineSlots = freeSlots.filter(slot => slot[1] === nine).length;
   const tenSlots = freeSlots.filter(slot => slot[1] === ten).length;
