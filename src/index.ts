49a404807bc629b039a5dd9df299f5bef1259a09
Fabiano Brito <fabiano.brito@wolt.com>
2020-11-11 22:03:36 +0200

    Remove node notifier and only open browser window when slot is found

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import axios from 'axios';
-import notifier from 'node-notifier';
 import open from 'open';
 import moment from 'moment';
 import skipTimes from '../skip';
-import { logFreeTapiolaSlots, shouldNotifyFreeSlotsInTapiola } from './tapiola';
+import { shouldNotifyFreeSlotsInTapiola } from './tapiola';
 
 moment.locale('fi');
 
@@ -12,6 +10,7 @@ let notified = false;
 const searchDayFormat = 'YYYY-MM-DD';
 
 const scrapeTapiola = async () => {
+  console.log(`\n* Searching slots at ${moment().format('L LT')}`);
   const startMoment = moment().weekday(6); // first sunday
   const endMoment = moment().add(31, 'days'); // last day allowed to search
 
@@ -23,23 +22,14 @@ const scrapeTapiola = async () => {
     const notifyTapiola = await shouldNotifyFreeSlotsInTapiola(date, skipTimes);
 
     if (notifyTapiola) {
-      notifier.notify({
-        title: 'Courts available!',
-        message: `There are courts available on ${searchMoment.format('L')}`,
-        open: `https://vj.slsystems.fi/tennispuisto/ftpages/ft-varaus-table-01.php?laji=1&pvm=${date}&goto=0`,
-        timeout: 5,
-      });
-      notified = notifyTapiola;
+      await open(`https://vj.slsystems.fi/tennispuisto/ftpages/ft-varaus-table-01.php?laji=1&pvm=${date}&goto=0`);
+      process.exit(0);
     }
     searchMoment.add(1, 'week');
   }
 }
 
-notifier.on('timeout', async (notifierObject, options) => {
-  await open(options.open);
-  process.exit(0);
-});
-
 const oneMinute = 1000 * 60;
+
 scrapeTapiola();
 setInterval(scrapeTapiola, oneMinute * 5);
